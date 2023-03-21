9ecc4d6feca3ed635a8600509fd3cbe7f409fe02
bgroves28 <briannamg99@gmail.com>
2023-03-20 21:39:51 -0500

    document formatting
    I made all the files look pretty by formatting them with shift+alt+f

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
-  function myAlert() {
-    var txt;
-    if (confirm("Are you sure you want to cancel your ticket?")) {
-      txt = "You pressed OK!";
-    } else {
-      txt = "You pressed Cancel!";
-    }
-    document.getElementById("demo").innerHTML = txt;
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function myAlert() {
+  var txt;
+  if (confirm("Are you sure you want to cancel your ticket?")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
   }
+  document.getElementById("demo").innerHTML = txt;
+}
